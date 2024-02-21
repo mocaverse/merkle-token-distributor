c94abfb19a8ab42fc729f6f768a013eb9f5fb7a5
boyuanx <boyuanxu@usc.edu>
2024-02-21 01:13:55 -0800

    Update deployment script

--- scripts/01-deploy-upgradeable.ts
+++ scripts/01-deploy-upgradeable.ts
@@ -2,7 +2,7 @@ import {ethers, upgrades} from 'hardhat'
 
 async function main() {
     const Factory = await ethers.getContractFactory('${ContractName}')
-    const instance = await upgrades.deployProxy(Factory, [])
+    const instance = await upgrades.deployProxy(Factory, ['${projectId}'])
     await instance.waitForDeployment()
 }
 
