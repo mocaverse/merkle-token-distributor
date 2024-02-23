0eb80234773cfcc87655a41c7e9afabaaa61a09f
Stone <stoneshi@ethsign.xyz>
2024-02-24 01:42:17 +0800

    feat: update nft name&symbol

--- scripts/03-deploy-dragonyearnft.ts
+++ scripts/03-deploy-dragonyearnft.ts
@@ -2,7 +2,10 @@ import {ethers, upgrades} from 'hardhat'
 
 async function main() {
     const Factory = await ethers.getContractFactory('DragonYearNFT')
-    const instance = await upgrades.deployProxy(Factory, ['Name', 'Symbol'])
+    const instance = await upgrades.deployProxy(Factory, [
+        'SIGN Lucky Dragon',
+        'LUCK'
+    ])
     await instance.waitForDeployment()
 }
 
