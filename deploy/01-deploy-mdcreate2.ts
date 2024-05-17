6dc138877a91e7f53a5d00df208419085300c87d
Jack Xu <jxu@ethsign.xyz>
2024-05-16 18:41:01 -0700

    Added expiry bypass & comments

--- deploy/01-deploy-mdcreate2.ts
+++ deploy/01-deploy-mdcreate2.ts
@@ -43,6 +43,15 @@ const deployMDCreate2: DeployFunction = async (
             waitConfirmations: 1
         }
     )
+    const nftGatedMerkleDistributorResult = await deploy(
+        'NFTGatedMerkleDistributor',
+        {
+            from: deployer,
+            log: true,
+            args: [],
+            waitConfirmations: 1
+        }
+    )
 
     const MDCreate2Factory = await ethers.getContractFactory('MDCreate2')
     const mdCreate2Instance = MDCreate2Factory.attach(
@@ -59,6 +68,10 @@ const deployMDCreate2: DeployFunction = async (
     await mdCreate2Instance.setImplementation(
         2,
         simpleERC721MerkleDistributorResult.address
+    )
+    await mdCreate2Instance.setImplementation(
+        3,
+        nftGatedMerkleDistributorResult.address
     )
 }
 
--- src/core/extensions/custom/NFTGatedMerkleDistributor.sol
+++ src/core/extensions/custom/NFTGatedMerkleDistributor.sol
@@ -5,24 +5,26 @@ import { TokenTableMerkleDistributor, TokenTableMerkleDistributorData } from "..
 import { DelegateXYZProvider } from "../../../utils/DelegateXYZProvider.sol";
 import { IERC721 } from "@openzeppelin/contracts/interfaces/IERC721.sol";
 
-struct MOCAMerkleDistributorData {
+struct NFTGatedMerkleDistributorData {
     TokenTableMerkleDistributorData base;
-    uint256 expiryTimestamp;
-    uint256 nftTokenId;
+    uint256 expiryTimestamp; // The airdrop can't be claimed past this timestamp. 0 == no expiry.
+    uint256 nftTokenId; // Used to infer the eligible claimer (aka current owner or an address authorized by owner
+        // through delegate.xyz)
 }
 
-contract MOCAMerkleDistributor is TokenTableMerkleDistributor, DelegateXYZProvider {
-    /// @custom:storage-location erc7201:ethsign.misc.MOCAMerkleDistributor
-    struct MOCAMerkleDistributorStorage {
+contract NFTGatedMerkleDistributor is TokenTableMerkleDistributor, DelegateXYZProvider {
+    /// @custom:storage-location erc7201:ethsign.misc.NFTGatedMerkleDistributor
+    struct NFTGatedMerkleDistributorStorage {
         address nft;
     }
 
-    // keccak256(abi.encode(uint256(keccak256("ethsign.misc.MOCAMerkleDistributor")) - 1)) & ~bytes32(uint256(0xff))
-    bytes32 private constant MOCAMerkleDistributorStorageLocation =
+    // keccak256(abi.encode(uint256(keccak256("ethsign.misc.NFTGatedMerkleDistributor")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 private constant NFTGatedMerkleDistributorStorageLocation =
         0x079e68f457f3a74750d0ad696a5ff6834ab64da74292dbd4ba0e15a12955a000;
 
+    // @dev Sets the NFT token address once
     function setNFT(address nft) external onlyOwner {
-        MOCAMerkleDistributorStorage storage $ = _getMOCAMerkleDistributorStorage();
+        NFTGatedMerkleDistributorStorage storage $ = _getNFTGatedMerkleDistributorStorage();
         if ($.nft != address(0)) revert UnsupportedOperation();
         $.nft = nft;
     }
@@ -46,26 +48,29 @@ contract MOCAMerkleDistributor is TokenTableMerkleDistributor, DelegateXYZProvid
     }
 
     function getNFT() external view returns (address) {
-        return _getMOCAMerkleDistributorStorage().nft;
+        return _getNFTGatedMerkleDistributorStorage().nft;
     }
 
     function delegateClaim(address, bytes32[] calldata, bytes32, bytes calldata) public payable virtual override {
         revert UnsupportedOperation();
     }
 
-    function decodeMOCALeafData(bytes memory data) public pure returns (MOCAMerkleDistributorData memory) {
-        return abi.decode(data, (MOCAMerkleDistributorData));
+    // @dev This replaces `decodeLeafData(...)` but needs to be a new function as the return types differ
+    function decodeMOCALeafData(bytes memory data) public pure returns (NFTGatedMerkleDistributorData memory) {
+        return abi.decode(data, (NFTGatedMerkleDistributorData));
     }
 
+    // @dev Deprecated in favor of `decodeMOCALeafData(...)`
     function decodeLeafData(bytes memory) public pure override returns (TokenTableMerkleDistributorData memory) {
         revert UnsupportedOperation();
     }
 
+    // @dev Disable fee collection for this customization
     // solhint-disable-next-line no-empty-blocks
     function _chargeFees(address, uint256) internal virtual override { }
 
     function _verifyAndClaim(
-        address,
+        address, // The recipient address is not used since it's inferred from the NFT token owner
         bytes32[] calldata proof,
         bytes32 group,
         bytes calldata data
@@ -77,17 +82,18 @@ contract MOCAMerkleDistributor is TokenTableMerkleDistributor, DelegateXYZProvid
         bytes32 leaf = encodeLeaf(address(0), group, data);
         verify(proof, leaf);
         _getBaseMerkleDistributorStorage().usedLeafs[leaf] = true;
-        MOCAMerkleDistributorData memory decodedData = decodeMOCALeafData(data);
+        NFTGatedMerkleDistributorData memory decodedData = decodeMOCALeafData(data); // Decode with the new function
         if (
-            decodedData.base.claimableTimestamp > block.timestamp || decodedData.expiryTimestamp < block.timestamp
+            decodedData.base.claimableTimestamp > block.timestamp
+                || (decodedData.expiryTimestamp < block.timestamp && decodedData.expiryTimestamp > 0)
                 || decodedData.base.claimableTimestamp < _getBaseMerkleDistributorStorage().startTime
         ) revert OutsideClaimableTimeRange();
-        address nftOwner = IERC721(_getMOCAMerkleDistributorStorage().nft).ownerOf(decodedData.nftTokenId);
+        address nftOwner = IERC721(_getNFTGatedMerkleDistributorStorage().nft).ownerOf(decodedData.nftTokenId);
         if (
             nftOwner != _msgSender()
                 && !externalDelegateRegistry.checkDelegateForContract(
                     _msgSender(), nftOwner, address(this), this.claim.selector
-                )
+                ) // Checking for delegation using delegate.xyz contract
         ) {
             revert UnsupportedOperation();
         }
@@ -95,9 +101,13 @@ contract MOCAMerkleDistributor is TokenTableMerkleDistributor, DelegateXYZProvid
         return decodedData.base.claimableAmount;
     }
 
-    function _getMOCAMerkleDistributorStorage() internal pure returns (MOCAMerkleDistributorStorage storage $) {
+    function _getNFTGatedMerkleDistributorStorage()
+        internal
+        pure
+        returns (NFTGatedMerkleDistributorStorage storage $)
+    {
         assembly {
-            $.slot := MOCAMerkleDistributorStorageLocation
+            $.slot := NFTGatedMerkleDistributorStorageLocation
         }
     }
 }
